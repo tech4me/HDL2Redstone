78e4c4c581e4b306ebd898796c6f825c10c9972a
tech4me <guiwanglong@gmail.com>
2019-10-19 19:36:04 -0400

    Small fixup for Liberty lib generation

--- HDL2Redstone/script/CreateLibertyLib.py
+++ HDL2Redstone/script/CreateLibertyLib.py
@@ -9,13 +9,13 @@ def create_liberty_lib(json_file, output_file):
         indent_level = 2
         write_line(output_file_f, 0, "library(HDL2Redstone) {")
         for cell in json_lib:
-            write_line(output_file_f, 1, "cell({cell_name}) {{".format(cell_name=cell["name"]))
-            for pin_name, pin_data in cell["pins"].items():
-                if cell["liberty"]:
+            if cell["liberty"]:
+                write_line(output_file_f, 1, "cell({cell_name}) {{".format(cell_name=cell["name"]))
+                for pin_name, pin_data in cell["pins"].items():
                     write_line(output_file_f, 2, "pin({pin_name}) {{".format(pin_name=pin_name))
                     write_line(output_file_f, 3, "direction: {direction};".format(direction=pin_data["direction"]))
                     if "function" in pin_data:
                         write_line(output_file_f, 3, "function: \"{function}\";".format(function=pin_data["function"]))
                     write_line(output_file_f, 2, "}")
-            write_line(output_file_f, 1, "}")
+                write_line(output_file_f, 1, "}")
         write_line(output_file_f, 0, "}")
