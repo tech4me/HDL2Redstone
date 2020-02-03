98e9ba8e45cbb60e7d8e66e9ae5fc6240efc476d
tech4me <guiwanglong@gmail.com>
2020-02-03 00:34:09 -0500

    Updated json submodule

--- HDL2Redstone/include/DesignConstraint.hpp
+++ HDL2Redstone/include/DesignConstraint.hpp
@@ -8,6 +8,7 @@
 namespace HDL2Redstone {
 class DesignConstraint {
   public:
+    bool readDCFile(const std::string& File_);
     void addForcedPlacement(const std::string& Name_, const Placement& P_) { ForcedPlacement.emplace(Name_, P_); }
     const std::map<std::string, Placement>& getForcedPlacement() const { return ForcedPlacement; }
 
--- HDL2Redstone/src/DesignConstraint.cpp
+++ HDL2Redstone/src/DesignConstraint.cpp
@@ -1,3 +1,14 @@
 #include <DesignConstraint.hpp>
 
 using namespace HDL2Redstone;
+
+bool DesignConstraint::readDCFile(const std::string& File_) {
+    /*
+    INIReader Reader(File_);
+    if (Reader.ParserError()) {
+        std::cout << "Error parsing: " << File_ << std::endl;
+        return true;
+    }
+    */
+    return false;
+}
